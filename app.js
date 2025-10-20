37eb1976ffe762eb88d35d67b55ddce80dfc590f
Emmanuel Hailu <mannysmart35@gmail.com>
2025-10-20 19:40:18 -0400

    Added Route

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const authRouter = require('./routes/auth')
+
+const authRouter = require('./routes/auth');
+const notesRouter = require('./routes/notes');
+
+const authenticateToken = require('./middleware/auth-middleware');
 require('dotenv').config();
     // import JWT routes
     // import resource API routes
@@ -27,11 +31,27 @@ app.use('/auth', authRouter);
 
 // Routes
 // AUTH JWT routes & Notes JWT
+
+
+// Test protected route
+app.get('/protected', authenticateToken, (req,res)  => {
+    res.json({
+        message: 'You accessed a protected route',
+        user: req.user
+    });
+});
+
+// Notes route
+app.use('/notes', authenticateToken, notesRouter);
+
+
     // coordinates function and cxontroller 
     // app.use(path{config.prefix}, router fiule contents )
 app.get('/', (req, res) => {
     res.json({ message: 'Notes API is running!' });
 });
+
+
 
 // Middleware
     // Error handling middleare
