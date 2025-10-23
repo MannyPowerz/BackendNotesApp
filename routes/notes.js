4e7886fd77914ec742eb72709865427f1df8c424
Emmanuel Hailu <mannysmart35@gmail.com>
2025-10-23 11:25:36 -0400

    Added Update/Patch router for notes

--- routes/notes.js
+++ routes/notes.js
@@ -40,6 +40,50 @@ router.get('/', async (req, res) => {
 
 // Update a specifc note whihc could be edited (reaosn why ia m using pathc instead of put)
 router.patch('/notes/:id', async (req, res) => {
+    const noteId = req.params.id;
+    const userId = req.user.userId;
+    const { title, content } = req.body;
+
+
+    try {
+        // NoteId Verifcation
+        const noteIndex = notes.findIndex(note => note.id === noteId);
+
+        if (noteIndex === -1) {
+            return res.status(404).json({ message: 'Note not found' });
+        }
+
+        // Get the specific note object you will modify
+        const note = notes[noteIndex];
+
+        // Security checking Note belongs to User
+        if (note.userId !== userId)
+
+
+        // Check if 'title' was provided and update the note object
+        if (title !== undefined) {
+            note.title = title
+        }
+
+        // Check if 'content' was provided and update the note object
+        if (content !== undefined) {
+            note.content = content;
+        }
+
+        //Update specifc note through assinging it to updated content and title for note of specific user
+        user.notes[noteIdVerifcationIndex] = noteToUpdate
+
+        res.status(200).json({
+            success: true,
+            message: 'Note updated',
+            data: noteToUpdate
+        })
+    }
+
+    catch (error) {
+        console.error({error: 'Error Updating the note: ', error});
+        res.status(500).json({error: 'Error updating the note.'})
+    }
 
 })
 
